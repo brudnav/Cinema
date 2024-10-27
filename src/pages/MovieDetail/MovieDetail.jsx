fa3b7c6377117eff8232dd71ea168fee76ef7206
Vaclav Brudna <brudna@cie-group.cz>
2024-10-27 22:19:57 +0100

    Initialization

---
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./MovieDetail.module.css"
+
+const MovieDetail = () => {
+    return (
+        <div>MovieDetail</div>
+    )
+}
+
+export default MovieDetail
---
+++ src/pages/Reservation/Reservation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Reservation.module.css"
+
+const Reservation = () => {
+    return (
+        <div>Reservation</div>
+    )
+}
+
+export default Reservation
