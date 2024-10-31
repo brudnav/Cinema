487efd55369fed280f2d278b6355d4e46afee5a0
brudnav <brudna@cie-group.cz>
2024-10-31 15:54:56 +0100

    layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Header from "./components/layout/Header/Header"
+import Footer from "./components/layout/Footer/Footer"
 function App() {
 
   return (
     <>
-      <nav>Navigace</nav>
+      <Header />
       <Outlet />
-      <footer>@2024</footer>
+      <Footer />
     </>
   )
 }
