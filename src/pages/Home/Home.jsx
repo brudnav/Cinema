f6b7d516e982d6b591922daa01a3116262a8f439
brudnav <brudna@cie-group.cz>
2024-10-31 17:37:34 +0100

    hero section, image slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import styles from "./Home.module.css"
-
+import joker from "../../assets/joker.jpg"
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div className={styles.container}>
+                <img className={styles.image} src={joker} />
+                <h1 style={{ marginTop: 20 }}>Joker</h1>
+                <div className={styles.information}>
+                    <span>2020</span>
+                    <span> . </span>
+                    <span>Crime, Drama, Thriller</span>
+                    <span> . </span>
+                    <span>Dolby Digital</span>
+                </div>
+            </div>
+            <div className={styles.controls}>
+                <span>⚫</span>
+                <span>⚫</span>
+                <span>⚫</span>
+            </div>
+            <h3>Promítáme a předprodej</h3>
+            <div className={styles.grid}>
+                <div className='item'></div>
+                <div className='item'></div>
+                <div className='item'></div>
+                <div className='item'></div>
+                <div className='item'></div>
+                <div className='item'></div>
+            </div>
+        </>
     )
 }
 
