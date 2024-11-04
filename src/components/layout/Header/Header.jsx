4567151e364fd29727f6db2943a5af7bb2a81334
brudnav <brudna@cie-group.cz>
2024-11-04 21:30:36 +0100

    connection between home and detail

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
     return (
         <Wrapper>
-
+            <Logo onClick={() => { navigate("/") }} />
         </Wrapper>
     )
 }
@@ -12,6 +16,14 @@ const Wrapper = styled.header`
 height: 50px;
 width: 100%;
 background-color: #5C5C5C;
+`
+
+const Logo = styled.div`
+height: 50px;
+width: 50px;
+border-radius: 50%;
+background-color: crimson;
+cursor: pointer;
 `
 
 export default Header
